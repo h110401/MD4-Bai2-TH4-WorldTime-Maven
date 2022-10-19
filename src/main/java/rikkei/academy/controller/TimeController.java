89386695dd8683d459c1ce65158c56dcef42a99e
h110401 <hung110401@gmail.com>
2022-10-19 16:28:23 +0700

    th4

---
+++ src/main/java/rikkei/academy/controller/TimeController.java
@@ -0,0 +1,30 @@
+package rikkei.academy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+public class TimeController {
+
+    @GetMapping({"/", "/world-clock"})
+    public String getTimeByTimeZone(
+            Model model,
+            @RequestParam(name = "city", required = false, defaultValue = "Asia/Ho_Chi_Minh") String city
+    ) {
+        Date date = new Date();
+        TimeZone local = TimeZone.getDefault();
+        TimeZone locale = TimeZone.getTimeZone(city);
+        long locale_time = date.getTime() + (locale.getRawOffset() - local.getRawOffset());
+        date.setTime(locale_time);
+        model.addAttribute("date", date);
+        model.addAttribute("city", city);
+        return "index";
+    }
+
+}
